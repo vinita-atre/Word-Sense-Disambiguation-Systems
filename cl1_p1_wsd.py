8448fc38cf1ec41608dd726a42a10f2cdeca77cf
Vinita Atre <ingenue.vinita@gmail.com>
2017-09-08 18:36:22 -0400

    initial commit; add gitignore, data directory

---
+++ cl1_p1_wsd.py
@@ -0,0 +1,137 @@
+"""
+CMSC723 / INST725 / LING723 -- Fall 2016
+Project 1: Implementing Word Sense Disambiguation Systems
+"""
+
+
+"""
+ read one of train, dev, test subsets 
+ 
+ subset - one of train, dev, test
+ 
+ output is a tuple of three lists
+ 	labels: one of the 6 possible senses <cord, division, formation, phone, product, text >
+ 	targets: the index within the text of the token to be disambiguated
+ 	texts: a list of tokenized and normalized text input (note that there can be multiple sentences)
+
+"""
+import nltk 
+def read_dataset(subset):
+	labels = []
+	texts = []
+	targets = []
+	if subset in ['train', 'dev', 'test']:
+		with open('data/wsd_'+subset+'.txt') as inp_hndl:
+			for example in inp_hndl:
+				label, text = example.strip().split('\t')
+				text = nltk.word_tokenize(text.lower().replace('" ','"'))
+				if 'line' in text:
+					ambig_ix = text.index('line')
+				elif 'lines' in text:
+					ambig_ix = text.index('lines')
+				else:
+					ldjal
+				targets.append(ambig_ix)
+				labels.append(label)
+				texts.append(text)
+		return (labels, targets, texts)
+	else:
+		print '>>>> invalid input !!! <<<<<'
+
+"""
+computes f1-score of the classification accuracy
+
+gold_labels - is a list of the gold labels
+predicted_labels - is a list of the predicted labels
+
+output is a tuple of the micro averaged score and the macro averaged score
+
+"""
+import sklearn.metrics
+def eval(gold_labels, predicted_labels):
+	return ( sklearn.metrics.f1_score(gold_labels, predicted_labels, average='micro'),
+			 sklearn.metrics.f1_score(gold_labels, predicted_labels, average='macro') )
+
+
+"""
+a helper method that takes a list of predictions and writes them to a file (1 prediction per line)
+predictions - list of predictions (strings)
+file_name - name of the output file
+"""
+def write_predictions(predictions, file_name):
+	with open(file_name, 'w') as outh:
+		for p in predictions:
+			outh.write(p+'\n')
+
+"""
+Trains a naive bayes model with bag of words features and computes the accuracy on the test set
+
+train_texts, train_targets, train_labels are as described in read_dataset above
+The same thing applies to the reset of the parameters.
+"""
+def run_bow_naivebayes_classifier(train_texts, train_targets, train_labels, 
+				dev_texts, dev_targets,dev_labels, test_texts, test_targets, test_labels):
+	
+	"""
+	**Your final classifier implementation of part 2 goes here**
+	"""
+	pass
+
+"""
+Trains a perceptron model with bag of words features and computes the accuracy on the test set
+
+train_texts, train_targets, train_labels are as described in read_dataset above
+The same thing applies to the reset of the parameters.
+
+"""
+def run_bow_perceptron_classifier(train_texts, train_targets,train_labels, 
+				dev_texts, dev_targets,dev_labels, test_texts, test_targets, test_labels):
+	"""
+	**Your final classifier implementation of part 3 goes here**
+	"""
+	pass
+
+
+
+"""
+Trains a naive bayes model with bag of words features  + two additional features 
+and computes the accuracy on the test set
+
+train_texts, train_targets, train_labels are as described in read_dataset above
+The same thing applies to the reset of the parameters.
+
+"""
+def run_extended_bow_naivebayes_classifier(train_texts, train_targets,train_labels, 
+				dev_texts, dev_targets,dev_labels, test_texts, test_targets, test_labels):
+	"""
+	**Your final implementation of Part 4 with perceptron classifier**
+	"""
+	pass
+
+"""
+Trains a perceptron model with bag of words features  + two additional features 
+and computes the accuracy on the test set
+
+train_texts, train_targets, train_labels are as described in read_dataset above
+The same thing applies to the reset of the parameters.
+
+"""
+def run_extended_bow_perceptron_classifier(train_texts, train_targets,train_labels, 
+				dev_texts, dev_targets,dev_labels, test_texts, test_targets, test_labels):
+	"""
+	**Your final implementation of Part 4 with perceptron classifier**
+	"""
+	pass
+
+
+if __name__ == "__main__":
+    # reading, tokenizing, and normalizing data
+    train_labels, train_targets, train_texts = read_dataset('train')
+    dev_labels, dev_targets, dev_texts = read_dataset('dev')
+    test_labels, test_targets, test_texts = read_dataset('test')
+
+    #running the classifier
+    test_scores = run_naivebayes_classifier(train_texts, train_targets, train_labels, 
+				dev_texts, dev_targets, dev_labels, test_texts, test_targets, test_labels)
+
+    print test_scores
